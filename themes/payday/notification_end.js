98fc0b884dc95cf4a893024c9b5bb32b26efecee
George Tsatsis <george@ratelimited.me>
2022-08-28 21:33:30 +0300

    impl: added notification_start/notification_end scripts to payday theme

---
+++ themes/payday/notification_end.js
@@ -0,0 +1,11 @@
+﻿// NotificationAPIAnnotation: DO_NOT_APPEND_ENGINE_CALLBACK
+notificationApi_Indicator.classList.add("flicker");
+
+notificationApi_Banner.classList.remove("max");
+notificationApi_Banner.classList.remove("flickerbg");
+
+setTimeout(() => {
+    notificationApi_Indicator.classList.remove("flicker");
+    notificationApi_Indicator.style.visibility = "hidden";
+    engine.call("notificationDoneCallback");
+}, 2000);
---
+++ themes/payday/notification_start.js
@@ -0,0 +1,11 @@
+﻿notificationApi_Indicator.style.visibility = "visible";
+notificationApi_Indicator.classList.add("flicker");
+
+let documentRoot = document.querySelector(":root");
+documentRoot.style.setProperty("--custom-notification-color", notificationApi_currentColor)
+
+setTimeout(() => {
+    notificationApi_Indicator.classList.remove("flicker");
+    notificationApi_Banner.classList.add("max");
+    notificationApi_Banner.classList.add("flickerbg");
+}, 2000);
